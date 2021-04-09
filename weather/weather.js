4a735ee5ec094c1a2cca28042b94e6b69ec0eb50
kgiroi2 <78235504+kgiroi2@users.noreply.github.com>
2021-04-09 17:37:45 -0500

    Update weather.js

--- weather/weather.js
+++ weather/weather.js
@@ -1,5 +1,6 @@
 var map = L.map('map').setView([38, -95], 4);
-var basemapUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+var basemapUrl = 'https://services.arcgisonline.com/arcgis/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}'
+ ;
 var basemap = L.tileLayer(basemapUrl).addTo(map);
 var radarUrl = 'https://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi';
 var radarDisplayOptions = {
@@ -8,18 +9,21 @@ var radarDisplayOptions = {
   transparent: true
 };
 var radar = L.tileLayer.wms(radarUrl, radarDisplayOptions).addTo(map);
+
+// Get GeoJSON data from the NWS weather alerts API
 var weatherAlertsUrl = 'https://api.weather.gov/alerts/active?region_type=land';
 $.getJSON(weatherAlertsUrl, function(data) {
-  // add more code here later
+  L.geoJSON(data, {
+    // Color all alert polygons yellow, but color Severe polygons pink, color minor polygons green
+    style: function(feature){
+      var alertColor = 'yellow';
+      if (feature.properties.severity === 'Severe') alertColor = 'pink'; 
+      else (feature.properties.minor === 'minor') alertColor = 'green'
+      return { color: alertColor }
+    },
+    // Add a popup on each feature showing the NWS alert headline
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup(feature.properties.headline);
+    }
+  }).addTo(map);
 });
-L.geoJSON(data).addTo(map);
-L.geoJSON(data, {
-  style: { color: 'orange' },
-  onEachFeature: function(feature, layer) {
-  layer.bindPopup(feature.properties.headline);
-}}).addTo(map);
-style: function(feature){
-  var alertColor = 'orange';
-  if (feature.properties.severity === 'Severe') alertColor = 'red';
-  return { color: alertColor };
-},  
